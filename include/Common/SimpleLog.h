0b7f6b1cd12c02f7b853f8f7d6fc607019166a85
sy-c <o2.syc@cern.ch>
2016-09-28 17:01:05 +0200

    SimleLog class

---
+++ include/Common/SimpleLog.h
@@ -0,0 +1,57 @@
+/// \file SimpleLog.h
+/// \brief C++ interface implementing a simple logging format to a file
+///
+/// \author Sylvain Chapeland, CERN
+
+
+#ifndef SRC_SIMPLE_LOG_H
+#define SRC_SIMPLE_LOG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <memory>
+
+class SimpleLog {
+
+  public:
+  
+  // constructor
+  // \param logFilePath Path to log file. If NULL, using stdout/stderr.
+  SimpleLog(const char* logFilePath=NULL);
+
+  // destructor
+  ~SimpleLog();
+  
+  // Set (or change) the log file name. Previous file used is closed. New file is appended when already existing.
+  // \param logFilePath Path to log file. If NULL, using stdout/stderr.
+  int setLogFile(const char* logFilePath=NULL);
+
+
+
+  
+  // Log an info message.
+  // The message is formatted with timestamp and severity.
+  // 
+  // \param message Message, in a printf-like compatible format (with associated extra arguments)
+  // NB: attribute format for printf-like argument check -> 1st (invisible) arg is 'this'
+  int info(const char *message, ...) __attribute__ ((format (printf, 2,3)));
+
+  // Log an error message. See info().
+  int error(const char *message, ...) __attribute__ ((format (printf, 2,3)));
+  
+  // Log a warning message. See info().
+  int warning(const char *message, ...) __attribute__ ((format (printf, 2,3)));
+  
+  
+  // explicitely disable automatically generated methods
+  // disable copy constructor
+  SimpleLog(const SimpleLog&) =delete;
+  // disable copy assignment operator
+  SimpleLog& operator=(const SimpleLog&) =delete;
+  
+  private:
+  class Impl;                       // private class for implementation
+  std::unique_ptr<Impl> pImpl;      // handle to private class instance at runtime  
+};
+
+#endif /* SRC_SIMPLE_LOG_H */
---
+++ src/SimpleLog.cxx
@@ -0,0 +1,156 @@
+#include <Common/SimpleLog.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+class SimpleLog::Impl {
+  public:
+
+  // constructor
+  Impl();
+  
+  // destructor
+  ~Impl();
+
+  // list of possible message severity levels  
+  enum Severity {Info,Error,Warning}; 
+
+  // base log function, with printf-like format arguments
+  // \param severity  Message severity.
+  // \param message   Message content, printf-like format.
+  // \param ap        Variable list of arguments associated with message.
+  int logV(SimpleLog::Impl::Severity severity, const char *message, va_list ap);
+  
+  protected: 
+  FILE *fp; // descriptor to be used. If NULL, using stdout/stderr.
+  
+  friend class SimpleLog;
+};
+
+SimpleLog::Impl::Impl() {
+  fp=NULL;
+}
+
+SimpleLog::Impl::~Impl() {
+  if (fp!=NULL) {
+    fclose(fp);
+    fp=NULL;
+  }
+}
+
+
+int SimpleLog::Impl::logV(SimpleLog::Impl::Severity s, const char *message, va_list ap)
+{
+  char buffer[1024] = "";
+  size_t len = sizeof(buffer);
+  size_t ix = 0;
+ 
+  // timestamp (microsecond)
+  struct timeval tv;
+  double fullTimeNow=-1;
+  if(gettimeofday(&tv,NULL) == -1){
+    fullTimeNow = time(NULL);
+  } else {
+    fullTimeNow = (double)tv.tv_sec + (double)tv.tv_usec/1000000;
+  }
+  time_t now;
+  struct tm tm_str;
+  now = (time_t)fullTimeNow;
+  localtime_r(&now, &tm_str);
+  double fractionOfSecond=fullTimeNow-now;
+  ix+=strftime(&buffer[ix], len-ix, "%Y-%m-%d %T", &tm_str);
+  ix+=snprintf(&buffer[ix], len-ix, ".%.3lf\t",fractionOfSecond);
+  if (ix>len) { ix=len; }
+
+  if (s==Severity::Error) {
+    ix+=snprintf(&buffer[ix], len-ix, "Error - ");
+  } else if (s==Severity::Warning) {
+    ix+=snprintf(&buffer[ix], len-ix, "Warning - ");
+  }
+
+  ix+=vsnprintf(&buffer[ix], len-ix, message, ap);
+  if (ix>len) { ix=len; } 
+  buffer[ix]=0;
+
+  FILE *fpOut=stdout;
+  
+  if (fp==NULL) {
+    if (s==Severity::Error) {
+      fpOut=stderr;
+    }
+  } else {
+    fpOut=fp;
+  }
+  fprintf(fpOut,"%s\n", buffer);
+  fflush(fpOut);
+  return 0;
+}
+
+
+
+
+
+
+SimpleLog::SimpleLog(const char* logFilePath) {
+  pImpl=std::make_unique<SimpleLog::Impl>();
+  if (pImpl==NULL) { throw __LINE__; }
+  setLogFile(logFilePath);
+}
+
+SimpleLog::~SimpleLog() {
+  setLogFile(NULL);
+}
+
+int SimpleLog::setLogFile(const char* logFilePath) {
+  if (pImpl->fp!=NULL) {
+    fclose(pImpl->fp);
+    pImpl->fp=NULL;
+  }
+  if (logFilePath!=NULL) {
+    pImpl->fp=fopen(logFilePath,"a");
+    if (pImpl->fp==NULL) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+
+int SimpleLog::info(const char *message, ...)
+{
+  int err = 0;
+
+  va_list ap;
+  va_start(ap, message);
+  err = pImpl->logV(Impl::Severity::Info,message, ap);
+  va_end(ap);
+
+  return err;
+}
+
+int SimpleLog::error(const char *message, ...)
+{
+  int err = 0;
+
+  va_list ap;
+  va_start(ap, message);
+  err = pImpl->logV(Impl::Severity::Error,message, ap);
+  va_end(ap);
+
+  return err;
+}
+
+int SimpleLog::warning(const char *message, ...)
+{
+  int err = 0;
+
+  va_list ap;
+  va_start(ap, message);
+  err = pImpl->logV(Impl::Severity::Warning,message, ap);
+  va_end(ap);
+
+  return err;
+}
+
+
+/// \todo: thread to flush output every 1 second
