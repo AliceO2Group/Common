46cddf414ddfcff4b04ecf6068d39476b6667453
Pascal Boeschoten <passiepascal@gmail.com>
2017-04-04 13:57:58 +0200

    Moved RORC System utilities to Common; Partially moved RORC Program to Common

--- include/Common/Exception.h
+++ include/Common/Exception.h
@@ -18,6 +18,8 @@ namespace ErrorInfo
 using Message = boost::error_info<struct _Message, std::string>;
 using Input = boost::error_info<struct _Input, std::string>;
 using Suffix = boost::error_info<struct _Suffix, std::string>;
+using FileName = boost::error_info<struct _FileName, std::string>;
+using FilesystemType = boost::error_info<struct _FileSystemType, std::string>;
 }
 
 struct Exception : virtual std::exception, virtual boost::exception
@@ -25,6 +27,8 @@ struct Exception : virtual std::exception, virtual boost::exception
     /// The what() function is overridden to use the 'ErrorInfo::Message' when available
     virtual const char* what() const noexcept override;
 };
+
+struct ProgramOptionException : virtual Exception {};
 
 }
 }
---
+++ include/Common/Program.h
@@ -0,0 +1,78 @@
+/// \file Program.h
+/// \brief Definition of the Program class.
+///
+/// \author Pascal Boeschoten (pascal.boeschoten@cern.ch)
+
+#pragma once
+
+#include <atomic>
+#include <boost/program_options.hpp>
+
+namespace AliceO2
+{
+namespace Common
+{
+
+/// Helper class for making a command-line program. It handles:
+/// - Creation of the options_descripotion object
+/// - Creation of the variables_map object
+/// - Help message
+/// - Exceptions & error messages
+/// - SIGINT signals
+class Program
+{
+  public:
+    Program();
+    virtual ~Program();
+
+    /// Execute the program using the given arguments
+    int execute(int argc, char** argv);
+
+    /// Returns true if the SIGINT signal been given (e.g. with Ctrl-c in a terminal)
+    static bool isSigInt(std::memory_order memoryOrder = std::memory_order_relaxed)
+    {
+      return sFlagSigInt.load(memoryOrder);
+    }
+
+    /// Get the atomic interrupt flag
+    static const std::atomic<bool>& getInterruptFlag()
+    {
+      return sFlagSigInt;
+    }
+
+  protected:
+    /// Should output be verbose
+    bool isVerbose() const
+    {
+      return mVerbose;
+    }
+
+    struct Description
+    {
+        std::string name; ///< Name of the utility
+        std::string description; ///< Short description
+        std::string usage; ///< Usage example
+    };
+
+  private:
+    /// Get the description of the program
+    /// \return A tuple containing name, description and example usage of the program
+    virtual Description getDescription() = 0;
+
+    /// Add the program's options
+    virtual void addOptions(boost::program_options::options_description& optionsDescription) = 0;
+
+    /// The main function of the program
+    virtual void run(const boost::program_options::variables_map& variablesMap) = 0;
+
+    static void sigIntHandler(int);
+
+    void printHelp(const boost::program_options::options_description& optionsDescription);
+
+    static std::atomic<bool> sFlagSigInt;
+
+    bool mVerbose;
+};
+
+} // namespace Common
+} // namespace AliceO2
