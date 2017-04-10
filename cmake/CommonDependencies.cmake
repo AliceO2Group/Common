6b671ed077f9572227817dfe407d11eec72c63f7
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-04-10 09:11:33 +0200

    Common: moved to O2 CMake

---
+++ cmake/CommonDependencies.cmake
@@ -0,0 +1,15 @@
+find_package(Boost COMPONENTS unit_test_framework program_options REQUIRED)
+find_package(Git QUIET)
+find_package(TCL)
+
+
+o2_define_bucket(
+  NAME
+  o2_common_bucket
+
+  DEPENDENCIES
+  ${Boost_PROGRAM_OPTIONS_LIBRARY}
+
+  SYSTEMINCLUDE_DIRECTORIES
+  ${Boost_INCLUDE_DIRS}
+)
