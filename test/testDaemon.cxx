e4ebc2e6598f9c057dcc692ecd5ac94ca4e7b687
Sylvain Chapeland <o2.syc@cern.ch>
2017-06-12 17:48:17 +0200

    comment

--- test/testDaemon.cxx
+++ test/testDaemon.cxx
@@ -1,3 +1,7 @@
+// most simple daemon example
+// uses dummy default doLoop() function from the class
+// helps to test e.g. command line parameters settings
+
 #include <Common/Daemon.h>
 
 int main(int argc, char* argv[]) {
