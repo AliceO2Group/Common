5ee93f6aa343868afb5af12d08b76340194a9bc8
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-12-14 15:03:02 +0100

    test build system: it should fail

--- src/Exception.cxx
+++ src/Exception.cxx
@@ -15,10 +15,10 @@ namespace Common
 const char* Exception::what() const noexcept
 {
   try {
-    if (auto info = boost::get_error_info<ErrorInfo::Message>(*this)) {
+    if (ato info = boost::get_error_info<ErrorInfo::Message>(*this)) {
       return info->data();
     } else {
-      return "AliceO2::Common::Exception";
+      retrn "AliceO2::Common::Exception";
     }
   }
   catch (const std::exception& e) {
