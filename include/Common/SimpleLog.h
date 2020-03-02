91e1e622217a39f25f8d6e053134d3b8cec0c124
Sylvain Chapeland <o2.syc@cern.ch>
2020-03-02 16:06:19 +0100

    simplelog: added log rotate + clang format cleanup

--- include/Common/SimpleLog.h
+++ include/Common/SimpleLog.h
@@ -3,7 +3,6 @@
 ///
 /// \author Sylvain Chapeland, CERN
 
-
 #ifndef SRC_SIMPLE_LOG_H
 #define SRC_SIMPLE_LOG_H
 
@@ -11,20 +10,25 @@
 #include <stdarg.h>
 #include <memory>
 
-class SimpleLog {
+class SimpleLog
+{
 
-  public:
-  
+ public:
   // constructor
   // \param logFilePath Path to log file. If NULL, using stdout/stderr.
-  SimpleLog(const char* logFilePath=NULL);
+  SimpleLog(const char* logFilePath = NULL);
 
   // destructor
   ~SimpleLog();
-  
+
   // Set (or change) the log file name. Previous file used is closed. New file is appended when already existing.
+  // Optionnaly, an automatic log rotation can be defined. Older files are renamed, appending .1, .2, .3, etc.
   // \param logFilePath Path to log file. If NULL, using stdout/stderr.
-  int setLogFile(const char* logFilePath=NULL);
+  // \param rotateMaxBytes Maximum file size, after which a new file is created. If zero, no limit.
+  // \param rotateMaxFiles Maximum number of files to keep (including the "current" file). If zero, no limit.
+  // \param rotateNow If non-zero, the file is immediately rotated (independently of its size), otherwise it is appended.
+  int setLogFile(const char* logFilePath = NULL,
+                 unsigned long rotateMaxBytes = 0, unsigned int rotateMaxFiles = 0, unsigned int rotateNow = 0);
 
   // Change file descriptors used for stdout/stderr with provided ones
   // They must be valid for the lifetime of this object (or until overwritten), and are not closed.
@@ -36,33 +40,32 @@ class SimpleLog {
     ShowSeveritySymbol = 0x4,
     ShowMessage = 0x8
   };
-  
+
   // Set output format based on (possibly OR-ed) format options from FormatOption enum
   void setOutputFormat(int opts);
-  
+
   // Log an info message.
   // The message is formatted with timestamp and severity.
-  // 
+  //
   // \param message Message, in a printf-like compatible format (with associated extra arguments)
   // NB: attribute format for printf-like argument check -> 1st (invisible) arg is 'this'
-  int info(const char *message, ...) __attribute__ ((format (printf, 2,3)));
+  int info(const char* message, ...) __attribute__((format(printf, 2, 3)));
 
   // Log an error message. See info().
-  int error(const char *message, ...) __attribute__ ((format (printf, 2,3)));
-  
+  int error(const char* message, ...) __attribute__((format(printf, 2, 3)));
+
   // Log a warning message. See info().
-  int warning(const char *message, ...) __attribute__ ((format (printf, 2,3)));
-  
-  
+  int warning(const char* message, ...) __attribute__((format(printf, 2, 3)));
+
   // explicitely disable automatically generated methods
   // disable copy constructor
-  SimpleLog(const SimpleLog&) =delete;
+  SimpleLog(const SimpleLog&) = delete;
   // disable copy assignment operator
-  SimpleLog& operator=(const SimpleLog&) =delete;
-  
-  private:
-  class Impl;                       // private class for implementation
-  std::unique_ptr<Impl> pImpl;      // handle to private class instance at runtime  
+  SimpleLog& operator=(const SimpleLog&) = delete;
+
+ private:
+  class Impl;                  // private class for implementation
+  std::unique_ptr<Impl> pImpl; // handle to private class instance at runtime
 };
 
 #endif /* SRC_SIMPLE_LOG_H */
